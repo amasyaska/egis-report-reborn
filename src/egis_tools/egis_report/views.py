3bf8cf9b25689e1229fc171bac4598b866cf521b
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-07-18 15:03:01 +0300

    added index html, corresponding index() view in egis_report views modified egis_tools urls.py to be able to access egis_report urls

--- src/egis_tools/egis_report/views.py
+++ src/egis_tools/egis_report/views.py
@@ -1,3 +1,24 @@
+import os
+
+from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    if (request.method == "GET"):
+        basepath = os.path.dirname(__file__)                            # get absolute path to this file
+        filepath = os.path.abspath(os.path.join(basepath, "templates", "index.html"))
+        with open(filepath, 'r') as f:
+            return HttpResponse(f.read())
+    elif (request.method == "POST"):
+        response = HttpResponse(status=200)
+        response["Location"] = "report/"
+        return response
+
+def report(request):
+    if (request.method == "GET"):
+        basepath = os.path.dirname(__file__)                            # get absolute path to this file
+        filepath = os.path.abspath(os.path.join(basepath, "templates", "report.html"))
+        with open(filepath, 'r') as f:
+            return HttpResponse(f.read())       
