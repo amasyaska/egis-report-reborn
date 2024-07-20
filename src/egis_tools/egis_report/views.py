6f7e5292fc4807f61beccf43158db170e9e084f7
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-07-20 20:16:55 +0300

    modified views to return next state of page on POST request, it seems to work

--- src/egis_tools/egis_report/views.py
+++ src/egis_tools/egis_report/views.py
@@ -3,6 +3,8 @@ import os
 from django.http import HttpResponse
 from django.shortcuts import render
 
+from . import functions
+
 # Create your views here.
 
 def index(request):
@@ -12,13 +14,14 @@ def index(request):
         with open(filepath, 'r') as f:
             return HttpResponse(f.read())
     elif (request.method == "POST"):
-        response = HttpResponse(status=200)
-        response["Location"] = "report/"
-        return response
+        basepath = os.path.dirname(__file__)                            # get absolute path to this file
+        filepath = os.path.abspath(os.path.join(basepath, "templates", "index_files_input.html"))
+        with open(filepath, 'r') as f:
+            return HttpResponse(functions.get_report_html(int(request.POST['xlsx_files_amount'])))
 
 def report(request):
     if (request.method == "GET"):
         basepath = os.path.dirname(__file__)                            # get absolute path to this file
         filepath = os.path.abspath(os.path.join(basepath, "templates", "report.html"))
         with open(filepath, 'r') as f:
-            return HttpResponse(f.read())       
+            return HttpResponse(f.read()) 
