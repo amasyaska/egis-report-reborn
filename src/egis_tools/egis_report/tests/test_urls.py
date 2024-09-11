58028610b3dd68df7115f8ab7cbbe154cdf062c4
amasyaska <bogdan.kolesnikov12345@gmail.com>
2024-09-11 18:20:14 +0300

    added tests for urls

--- src/egis_tools/egis_report/tests/test_urls.py
+++ src/egis_tools/egis_report/tests/test_urls.py
@@ -1,7 +1,18 @@
 from django.test import SimpleTestCase
+from django.test import Client
+from django.urls import reverse, resolve
+from egis_report.views import index, report
 
 
 class TestUrls(SimpleTestCase):
+
+    def test_main_page_reverse(self):
+        self.assertEqual(reverse("main"), "/")
+
+    def test_main_page_resolve(self):
+        self.assertIs(resolve("/").func, index)
     
-    def test_urls(self):
-        assert 1 == 2
+    def test_GET_main_page(self):
+        client = Client()
+        response = client.get(reverse("main"))
+        self.assertEqual(response.status_code, 200)     # successful
